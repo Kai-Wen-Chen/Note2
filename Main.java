a3ef5ff34b99216f1913dec2deeed38ab4f34227
陳楷文 <kelvin91734@yahoo.com.tw>
2016-07-26 12:04:07 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package note2;
+
+public class Main {
+	public static void main(String args[]){
+		System.out.println("Note 2: 跑计");
+		System.out.println("int ノ秈秈(0秨繷)せ秈(0x秨繷)ボ");
+		int i = 2;
+		int j = 077; //0秨繷ボ秈
+		int k = 0xBAAC; // 0x秨繷ボせ秈
+		System.out.println("i = 2 = " + i);
+		System.out.println("j = 077 = " + j);
+		System.out.println("k = 0xBAAC = " + k);
+		
+		System.out.println();
+		
+		System.out.println("long 惠璶计程Lボ");
+		long l = 300L;
+		System.out.println("l = 300L = " + l);
+		
+		System.out.println();
+		
+		System.out.println("double 计ボ弘絋");
+		double m = 4.02E23;
+		double n = 123.4E+306D;
+		System.out.println("m = 4.02E23 = " + m);
+		System.out.println("n = 123.4E+306D = " + n);
+	}
+}
